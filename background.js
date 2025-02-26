659fe8c3359ea7c9b928aa280dff33b30ab0c717
Koy <inkyo.dev@gmail.com>
2025-02-26 14:35:14 +0900

    refactor: remove comment

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 chrome.runtime.onInstalled.addListener(() => {
-  // 처음 설치될 때 기본값을 설정
   chrome.storage.local.get(["hintEnabled"], function (result) {
     if (result.hintEnabled === undefined) {
       chrome.storage.local.set({ hintEnabled: false });
